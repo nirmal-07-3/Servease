f25fb0a605eaed131a68f754f14afdd008df75b1
ASHUTOSH RATH <rathashutosh458@gmail.com>
2026-04-16 10:12:02 +0530

    main

--- src/main/java/com/servease/Main.java
+++ src/main/java/com/servease/Main.java
@@ -1,6 +1,7 @@
 package com.servease;
 
 import com.servease.ui.auth.LoginFrame;
+import com.servease.ui.auth.RegisterFrame;
 import com.servease.ui.dashboard.UpdateServiceFrame;
 
 public class Main {
